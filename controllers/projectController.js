1d778b6a3d3bf16755625c7d5462b455e8c9fa01
bscheucher <122296588+bscheucher@users.noreply.github.com>
2025-01-20 03:45:06 +0100

    reverse order in getProjects

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -2,7 +2,9 @@ import db from "../config/dbConfig.js";
 
 export const getProjects = async (req, res) => {
   try {
-    const { rows } = await db.query("SELECT * FROM portfolio_projects");
+    const { rows } = await db.query(
+      "SELECT * FROM portfolio_projects ORDER BY created_at DESC"
+    );
     res.render("projects.ejs", { projects: rows });
   } catch (err) {
     console.error("Error fetching projects:", err);
